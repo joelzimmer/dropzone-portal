371888a374e71152c73fc0391abbc211c6705601
Joel Zimmer <jzimmer@squarespace.com>
2022-06-16 15:33:30 -0400

    Use an absolutely positioned div

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 function App() {
   const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [isDivOpen, setIsDivOpen] = useState(false);
   const {getRootProps, getInputProps, open, acceptedFiles} = useDropzone();
 
   const files = acceptedFiles.map(file => (
@@ -15,15 +16,17 @@ function App() {
 
   return (
     <div className="App">
-      <header>
-        <button id="open-button" onClick={() => setIsPopoverOpen(true)}>Open Popover</button>
-      </header>
+      <div className="Header">
+        <button id="open-popover-button" onClick={() => setIsPopoverOpen(true)}>Open Popover</button>
+        <button id="open-div-button" onClick={() => setIsDivOpen(true)}>Open Div</button>
+        <button onClick={() => open()}>Just Open</button>
+      </div>
       <aside>
         <h4>Files</h4>
         <ul>{files}</ul>
       </aside>
       {isPopoverOpen && (
-        <Portal node={document.getElementById('open-button')}>
+        <Portal node={document.getElementById('open-popover-button')}>
           <div className="Popover">
             <div {...getRootProps()}>
               <input {...getInputProps()} />
@@ -35,6 +38,15 @@ function App() {
           </div>
         </Portal>
       )}
+      <div className="Popover" style={{ display: isDivOpen ? 'block' : 'none', top: 0, background: '#ccc' }}>
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <button onClick={() => {
+                open();
+                // setIsDivOpen(false);
+              }}>Upload</button>
+            </div>
+          </div>
 
     </div>
   );
